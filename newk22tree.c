6661db0ed2109b9ba5cc88a0d3c7439fb184f96a
john <johngrigo33@gmail.com>
2025-10-23 12:28:28 +0300

    new k22tre

---
+++ newk22tree.c
@@ -0,0 +1,93 @@
+#include <linux/k22info.h>
+#include <linux/signal.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+#include <linux/errno.h>
+
+struct stack_node{
+	struct task_struct *task; 
+	struct list_head list;
+};
+
+
+static int do_k22tree(struct k22info *buf, int *ne)
+{
+	int size;
+	int count = 0;
+	int ret_code;
+	struct k22info *kbuf;
+	struct stack_node *node;
+	struct stack_node *new;
+	struct list_head stack_head;
+
+
+	copy_from_user(&size, ne, sizeof(size));
+
+	kbuf = kmalloc(sizeof(struct k22info)*size, GFP_KERNEL);
+
+	stack_head = INIT_LIST_HEAD(&stack_head);
+
+	new = kmalloc(sizeof(struct stack_node), GFP_KERNEL);
+	INIT_LIST_HEAD(&new->list);
+
+	list_add(&new->list, &stack_head);
+
+	do{
+		
+		struct list_head *top = stack_head.next;
+
+		
+		list_del(top);
+		node = list_entry(top, struct stack_node, list);
+
+		// TBD we need to fill the k22info struct for it's node and add it to the buffer
+
+		struct task_struct *cur;
+		list_for_each_entry_reverse(cur, &node->task->children, children){
+
+			
+			new = kmalloc(sizeof(struct stack_node), GFP_KERNEL);
+			new->task = cur;
+			INIT_LIST_HEAD(&new->list);
+			
+			list_add(&new->list, &stack_head);
+		}
+		
+		struct task_struct *tmp_task;
+		struct task_struct *tmp_child;
+
+		tmp_task = node->task;
+
+		tmp_child = list_first_entry(tmp_task->children, struct task_struct, children);
+		kbuf[count].first_child_pid = tmp_child->pid;
+
+		tmp_child = list_last_entry(tmp_task->sibling, struct task_struct, children);
+		kbuf[count].next_sibling_pid = tmp_child->pid;
+
+		strcpy(kbuf[count].comm, tmp_task->comm);
+		kbuf[count].pid = tmp_task->pid;
+		kbuf[count].parent_pid = tmp_task->parent->pid;
+		kbuf[count].nvcsw = tmp_task->nvcsw;
+		kbuf[count].nivcsw = tmp_task->nivcsw;
+		kbuf[count].start_time = tmp_task->start_time;
+		count++;
+
+		kfree(node);
+
+
+	} while(!list_empty(&stack_head));
+
+	ret_code = count;
+
+	copy_to_user(buf, kbuf, count*sizeof(struct k22info));
+
+
+
+	return ret_code;
+}
+
+SYSCALL_DEFINE2(k22tree, struct k22info __user *, buf, int __user *, ne)
+{
+	return do_k22tree(buf, ne);
+}
