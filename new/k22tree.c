992b313fc4b6daa8b1184e030606b132cc2688ea
Xristos Giannakis <chrigiannakis09@gmail.com>
2025-10-30 14:07:33 +0200

    Comments

--- new/k22tree.c
+++ new/k22tree.c
@@ -10,12 +10,25 @@
 #include <linux/sched/signal.h>
 #include <linux/k22info.h>
 
+/**
+ * struct info_node - Helper struct used to implement stack for dfs
+ * @ list: pointer to the next member of the stack
+ * @ task: pointer to the task stored in current node 
+*/
 struct info_node {
 	struct list_head list;
 	struct task_struct *task;
 };
 
-/* Helper function to find first (oldest) child */
+/**
+ * first_child_pid() - Helper Function that fetched pid of the first child
+ * @ task: pointer to the task whose first child we need to get the pid of
+ * 
+ * Context: The function uses no locks and does not sleep 
+ *
+ * Return: The pid of the first child of the given task or 0 if there are
+ *         no children
+*/
 static pid_t find_first_child_pid(struct task_struct *task)
 {
 	struct task_struct *child;
@@ -23,10 +36,18 @@ static pid_t find_first_child_pid(struct task_struct *task)
 	list_for_each_entry(child, &task->children, sibling)
 		return task_pid_nr(child);
 
-	return 0; /* No children */
+	return 0;
 }
 
-/* Helper function to find the pid of the next sibling */
+/**
+ * find_next_sibling_pid() - Helper function to find the pid of the next sibling
+ * @ task: Pointer to the task whose first sibling we want to get the pid of 
+ *
+ * Context: The function uses no locks and does not sleep
+ * 
+ * Return: The pid of the next sibling of the given task  or 0 if there are 
+ *         no siblings. 
+*/
 static pid_t find_next_sibling_pid(struct task_struct *task)
 {
 	struct task_struct *next;
@@ -41,6 +62,24 @@ static pid_t find_next_sibling_pid(struct task_struct *task)
 	return task_pid_nr(next);
 }
 
+
+/**
+ * dfs() - Function that performs a dfs of the task list
+ * @ kbuf: pointer to a kernel allocated buffer that will store the info 
+ *				 from the nodes that we traverse
+ * @ max: Maximum number of tasks that can fit into the kbuf
+ *
+ * This funtion uses a stack to perform a non recursive depth first search
+ * of the task list with respect to parent child and sibling relashionships.
+ * The traversed task get some of the info saved in the kbuf in variables of
+ * type k22info (see linux/k22info.h). if there are more tasks (processes) 
+ * running than the kbuf can hold (max < num_processes) the function copies
+ * as many as possible into the kbuf and just counts the rest 
+ * 
+ * Return:
+ * * ret_val - Number of running processes (not necessarily as many as the kbuf has)
+ * * -ENOMEM - Memory allocation has failed
+ */
 static int dfs(struct k22info *kbuf, int max)
 {
 	int count = 0;
@@ -137,8 +176,8 @@ leave:
 
 /**
  * do_k22tree() - System call that fetches information about running processes
- * @buf: Pointer to user space buffer that will store info about the processes
- * @ne: Pointer to user space int that determines how many entities the buf can hold
+ * @ buf: Pointer to user space buffer that will store info about the processes
+ * @ ne: Pointer to user space int that determines how many entities the buf can hold
  *
  * The system call fetches and exposes process-specific information to user space
  * about currently running processes by performing a Depth First Search (DFS)
@@ -161,9 +200,9 @@ leave:
  *
  * Return:
  * * ret_val - Total number of processes in the system.
- * * -EFAULT - If buf or ne are located in inaccessible user address space.
- * * -EINVAL - If buf or ne are NULL or *ne < 1.
- * * -ENOMEM - If memory allocation fails.
+ * * -EFAULT - buf or ne are located in inaccessible user address space.
+ * * -EINVAL - buf or ne are NULL or *ne < 1.
+ * * -ENOMEM - Memory allocation fails.
  */
 static int do_k22tree(struct k22info *buf, int *ne)
 {
