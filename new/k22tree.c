d6e246e02d2ea4e7c611e90eb21a6d16c886e80c
zang <johngrigo33@gmail.com>
2025-11-05 12:08:33 +0200

    stopped alocating the buffer based on ne and some other minor chagnes

--- new/k22tree.c
+++ new/k22tree.c
@@ -16,8 +16,8 @@
  * @ task: pointer to the task stored in current node
  */
 struct info_node {
-  struct list_head list;
-  struct task_struct *task;
+	struct list_head list;
+	struct task_struct *task;
 };
 
 /*
@@ -29,13 +29,15 @@ struct info_node {
  * Return: The pid of the first child of the given task or 0 if there are
  *         no children
  */
-static pid_t find_first_child_pid(struct task_struct *task) {
-  struct task_struct *child;
+static pid_t find_first_child_pid(struct task_struct *task)
+{
+	struct task_struct *child;
 
-  list_for_each_entry(child, &task->children,
-                      sibling) return task_pid_nr(child);
+	list_for_each_entry(child, &task->children, sibling) {
+		return task_pid_nr(child);
+	}
 
-  return 0;
+	return 0;
 }
 
 /*
@@ -47,17 +49,18 @@ static pid_t find_first_child_pid(struct task_struct *task) {
  * Return: The pid of the next sibling of the given task  or 0 if there are
  *         no siblings.
  */
-static pid_t find_next_sibling_pid(struct task_struct *task) {
-  struct task_struct *next;
+static pid_t find_next_sibling_pid(struct task_struct *task)
+{
+	struct task_struct *next;
 
-  if (!task->real_parent || list_empty(&task->real_parent->children))
-    return 0;
+	if (!task->real_parent || list_empty(&task->real_parent->children))
+		return 0;
 
-  if (list_is_last(&task->sibling, &task->real_parent->children))
-    return 0;
+	if (list_is_last(&task->sibling, &task->real_parent->children))
+		return 0;
 
-  next = list_next_entry(task, sibling);
-  return task_pid_nr(next);
+	next = list_next_entry(task, sibling);
+	return task_pid_nr(next);
 }
 
 /*
@@ -78,93 +81,72 @@ static pid_t find_next_sibling_pid(struct task_struct *task) {
  * has)
  * * -ENOMEM - Memory allocation has failed
  */
-static int dfs(struct k22info *kbuf, int max) {
-  int count = 0;
-  int ret_val = 0;
-  struct info_node *curr;
-  bool lock = false;
-  LIST_HEAD(stack);
+static int dfs(struct k22info *kbuf, int max)
+{
+	int count = 0;
+	int ret_val = 0;
+	struct info_node *curr;
+	struct info_node *root;
 
-  struct info_node *root = kmalloc(sizeof(*root), GFP_KERNEL);
+	root = kmalloc(sizeof(*root), GFP_KERNEL);
+	if (!root) {
+		ret_val = -ENOMEM;
+		goto leave;
+	}
 
-  if (!root) {
-    ret_val = -ENOMEM;
-    goto leave;
-  }
+	LIST_HEAD(stack);
 
-  root->task = &init_task;
-  INIT_LIST_HEAD(&root->list);
-  list_add(&root->list, &stack);
+	root->task = &init_task;
+	INIT_LIST_HEAD(&root->list);
+	list_add(&root->list, &stack);
 
-  read_lock(&tasklist_lock);
-  lock = true;
+	read_lock(&tasklist_lock);
+	while (!list_empty(&stack) && count < max) {
 
-  while (!list_empty(&stack)) {
-    curr = list_last_entry(&stack, struct info_node, list);
-    list_del(&curr->list);
+		curr = list_last_entry(&stack, struct info_node, list);
+		list_del(&curr->list);
 
-    if (count >= max) {
-      kfree(curr);
-      goto counting;
-    }
+		kbuf[count].pid = task_pid_nr(curr->task);
+		kbuf[count].parent_pid = task_ppid_nr(curr->task);
+		get_task_comm(kbuf[count].comm, curr->task);
+		kbuf[count].first_child_pid = find_first_child_pid(curr->task);
+		kbuf[count].next_sibling_pid = find_next_sibling_pid(curr->task);
+		kbuf[count].nvcsw = curr->task->nvcsw;
+		kbuf[count].nivcsw = curr->task->nivcsw;
+		kbuf[count].start_time = curr->task->start_time;
+		count++;
 
-    kbuf[count].pid = task_pid_nr(curr->task);
-    kbuf[count].parent_pid = task_ppid_nr(curr->task);
-    get_task_comm(kbuf[count].comm, curr->task);
+		struct task_struct *child;
+		list_for_each_entry_reverse(child, &curr->task->children, sibling) {
+			struct info_node *child_node;
 
-    kbuf[count].first_child_pid = find_first_child_pid(curr->task);
-    kbuf[count].next_sibling_pid = find_next_sibling_pid(curr->task);
+			child_node = kmalloc(sizeof(*child_node), GFP_ATOMIC);
+			if (!child_node) {
+				ret_val = -ENOMEM;
+				kfree(curr);
+				goto leave;
+			}
 
-    kbuf[count].nvcsw = curr->task->nvcsw;
-    kbuf[count].nivcsw = curr->task->nivcsw;
+			child_node->task = child;
+			INIT_LIST_HEAD(&child_node->list);
+			list_add_tail(&child_node->list, &stack);
+		}
 
-    kbuf[count].start_time = curr->task->start_time;
+		kfree(curr);
+	}
 
-    count++;
-
-    struct task_struct *child;
-
-    list_for_each_entry_reverse(child, &curr->task->children, sibling) {
-      struct info_node *child_node;
-
-      child_node = kmalloc(sizeof(*child_node), GFP_ATOMIC);
-      if (!child_node) {
-        ret_val = -ENOMEM;
-        kfree(curr);
-        goto free_mem;
-      }
-
-      child_node->task = child;
-      INIT_LIST_HEAD(&child_node->list);
-      list_add_tail(&child_node->list, &stack);
-    }
-
-    kfree(curr);
-  }
-
-  ret_val = count;
-  goto leave;
-
-counting: {
-  struct task_struct *t;
-
-  count = 0;
-  for_each_process(t) count++;
-  ret_val = count;
-}
-
-free_mem:
-  while (!list_empty(&stack)) {
-    struct info_node *node = list_last_entry(&stack, struct info_node, list);
-
-    list_del(&node->list);
-    kfree(node);
-  }
+	ret_val = count;
 
 leave:
-  if (lock)
-    read_unlock(&tasklist_lock);
-  return ret_val;
+	read_unlock(&tasklist_lock);
+
+	while (!list_empty(&stack)) {
+		struct info_node *node = list_last_entry(&stack, struct info_node, list);
+		list_del(&node->list);
+		kfree(node);
+	}
+
+	return ret_val;
 }
 
 /*
@@ -198,59 +180,68 @@ leave:
  * * -EINVAL - buf or ne are NULL or *ne < 1.
  * * -ENOMEM - Memory allocation fails.
  */
-static int do_k22tree(struct k22info *buf, int *ne) {
-  struct k22info *kbuf = NULL;
-  int size;
-  int ret_val = 0;
-  int number_processes;
-  int kne;
+static int do_k22tree(struct k22info *buf, int *ne)
+{
+	struct k22info *kbuf = NULL;
+	struct task_struct *tmp;
+	int size;
+	int ret_val = 0;
+	int processes_written;
+	int process_count;
+	int kbuf_size;
 
-  if (!buf || !ne) {
-    ret_val = -EINVAL;
-    goto out;
-  }
+	if (!buf || !ne) {
+		ret_val = -EINVAL;
+		goto out;
+	}
 
-  if (copy_from_user(&size, ne, sizeof(int))) {
-    ret_val = -EFAULT;
-    goto out;
-  }
+	if (copy_from_user(&size, ne, sizeof(int))) {
+		ret_val = -EFAULT;
+		goto out;
+	}
 
-  if (size < 1) {
-    ret_val = -EINVAL;
-    goto out;
-  }
+	if (size < 1) {
+		ret_val = -EINVAL;
+		goto out;
+	}
 
-  kbuf = kcalloc(size, sizeof(struct k22info), GFP_KERNEL);
-  if (!kbuf) {
-    ret_val = -ENOMEM;
-    goto out;
-  }
+	read_lock(&tasklist_lock);
+	for_each_process(tmp) {
+		process_count++;
+	}
+	read_unlock(&tasklist_lock);
 
-  number_processes = dfs(kbuf, size);
-  if (number_processes < 0) {
-    ret_val = number_processes;
-    goto out;
-  }
+	kbuf_size = min(size, process_count + 20);
+	kbuf = kcalloc(kbuf_size, sizeof(struct k22info), GFP_KERNEL);
+	if (!kbuf) {
+		ret_val = -ENOMEM;
+		goto free_mem;
+	}
 
-  kne = min(number_processes, size);
+	processes_written = dfs(kbuf, kbuf_size);
+	if (processes_written < 0) {
+		ret_val = processes_written;
+		goto free_mem;
+	}
 
-  if (copy_to_user(buf, kbuf, kne * sizeof(struct k22info))) {
-    ret_val = -EFAULT;
-    goto out;
-  }
+	if (copy_to_user(buf, kbuf, processes_written * sizeof(struct k22info))) {
+		ret_val = -EFAULT;
+		goto free_mem;
+	}
 
-  if (copy_to_user(ne, &kne, sizeof(int))) {
-    ret_val = -EFAULT;
-    goto out;
-  }
+	if (copy_to_user(ne, &processes_written, sizeof(int))) {
+		ret_val = -EFAULT;
+		goto free_mem;
+	}
 
-  ret_val = number_processes;
+	ret_val = max(processes_written, process_count);
 
+free_mem:
+	kfree(kbuf);
 out:
-  kfree(kbuf);
-  return ret_val;
+	return ret_val;
 }
 
 SYSCALL_DEFINE2(k22tree, struct k22info __user *, buf, int __user *, ne) {
-  return do_k22tree(buf, ne);
+	return do_k22tree(buf, ne);
 }
