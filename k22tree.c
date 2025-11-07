3d24a247fd7ad2181ab0869978917433637949f9
zang <johngrigo33@gmail.com>
2025-11-07 10:03:31 +0200

    changed the ifs logic in the for loop

--- k22tree.c
+++ k22tree.c
@@ -11,6 +11,7 @@
 #include <linux/uaccess.h>
 
 #define SLACK 50
+#define MAX_ATTEMPTS 10
 /*
  * first_child_pid() - Helper Function that fetched pid of the first child
  * @ task: pointer to the task whose first child we need to get the pid of
@@ -21,12 +22,13 @@
  *         no children
  */
 static pid_t find_first_child_pid(struct task_struct *task) {
-  struct task_struct *child;
+	struct task_struct *child;
 
-  list_for_each_entry(child, &task->children,
-                      sibling) return task_pid_nr(child);
+	list_for_each_entry(child, &task->children, sibling) {
+		return task_pid_nr(child);
+	}
 
-  return 0;
+	return 0;
 }
 
 /*
@@ -39,16 +41,16 @@ static pid_t find_first_child_pid(struct task_struct *task) {
  *         no siblings.
  */
 static pid_t find_next_sibling_pid(struct task_struct *task) {
-  struct task_struct *next;
+	struct task_struct *next;
 
-  if (!task->real_parent || list_empty(&task->real_parent->children))
-    return 0;
+	if (!task->real_parent || list_empty(&task->real_parent->children))
+		return 0;
 
-  if (list_is_last(&task->sibling, &task->real_parent->children))
-    return 0;
+	if (list_is_last(&task->sibling, &task->real_parent->children))
+		return 0;
 
-  next = list_next_entry(task, sibling);
-  return task_pid_nr(next);
+	next = list_next_entry(task, sibling);
+	return task_pid_nr(next);
 }
 
 /*
@@ -72,56 +74,56 @@ static pid_t find_next_sibling_pid(struct task_struct *task) {
  * * ret_val - Number of running processes
  */
 static int dfs(struct k22info *kbuf, int max) {
-    struct task_struct *curr = &init_task;
-    int count_total = 0;
-    int count_stored = 0;
+	struct task_struct *curr = &init_task;
+	int count_total = 0;
+	int count_stored = 0;
 
-    read_lock(&tasklist_lock);
+	read_lock(&tasklist_lock);
 
-    while (curr) {
-        if (count_stored < max) {
-            kbuf[count_stored].pid = task_pid_nr(curr);
-            kbuf[count_stored].parent_pid = task_ppid_nr(curr);
-            get_task_comm(kbuf[count_stored].comm, curr);
-            kbuf[count_stored].first_child_pid = find_first_child_pid(curr);
-            kbuf[count_stored].next_sibling_pid = find_next_sibling_pid(curr);
-            kbuf[count_stored].nvcsw = curr->nvcsw;
-            kbuf[count_stored].nivcsw = curr->nivcsw;
-            kbuf[count_stored].start_time = curr->start_time;
-            count_stored++;
-        }
-        count_total++;
+	while (curr) {
+		if (count_stored < max) {
+			kbuf[count_stored].pid = task_pid_nr(curr);
+			kbuf[count_stored].parent_pid = task_ppid_nr(curr);
+			get_task_comm(kbuf[count_stored].comm, curr);
+			kbuf[count_stored].first_child_pid = find_first_child_pid(curr);
+			kbuf[count_stored].next_sibling_pid = find_next_sibling_pid(curr);
+			kbuf[count_stored].nvcsw = curr->nvcsw;
+			kbuf[count_stored].nivcsw = curr->nivcsw;
+			kbuf[count_stored].start_time = curr->start_time;
+			count_stored++;
+		}
+		count_total++;
 
-        if (!list_empty(&curr->children)) {
-            curr = list_first_entry(&curr->children, struct task_struct, sibling);
-            continue;
-        }
+		if (!list_empty(&curr->children)) {
+			curr = list_first_entry(&curr->children, struct task_struct, sibling);
+			continue;
+		}
 
-        struct task_struct *next = NULL;
-        
-        while (curr && !next) {
-            if (curr != &init_task && curr->real_parent) {
-                if (!list_is_last(&curr->sibling, &curr->real_parent->children)) {
-                    next = list_next_entry(curr, sibling);
-                }
-            }
-            
-            if (next) {
-                break; 
-            }
-            
-            if (curr == &init_task) {
-                curr = NULL; 
-            } else {
-                curr = curr->real_parent;
-            }
-        }
-        
-        curr = next; 
-    }
+		struct task_struct *next = NULL;
 
-    read_unlock(&tasklist_lock);
-    return count_total;
+		while (curr && !next) {
+			if (curr != &init_task && curr->real_parent) {
+				if (!list_is_last(&curr->sibling, &curr->real_parent->children)) {
+					next = list_next_entry(curr, sibling);
+				}
+			}
+
+			if (next) {
+				break; 
+			}
+
+			if (curr == &init_task) {
+				curr = NULL; 
+			} else {
+				curr = curr->real_parent;
+			}
+		}
+
+		curr = next; 
+	}
+
+	read_unlock(&tasklist_lock);
+	return count_total;
 }
 
 /*
@@ -157,81 +159,79 @@ static int dfs(struct k22info *kbuf, int max) {
  * * -EINVAL - buf or ne are NULL or *ne < 1.
  * * -ENOMEM - Memory allocation fails.
  */
-static int do_k22tree(struct k22info *buf, int *ne) {
-  struct k22info *kbuf = NULL;
-  struct task_struct *t;
-  int size;
-  int processes_before = 0;
-  int ret_val = 0;
-  int processes_after;
-  int kne;
+static int do_k22tree(struct k22info *buf, int *ne)
+{
+	struct k22info *kbuf = NULL;
+	struct task_struct *t;
+	int size;
+	int processes_before = 0;
+	int ret_val = 0;
+	int processes_after;
+	int kne;
 
-   if (!buf || !ne) {
-    ret_val = -EINVAL;
-    goto out;
-  }
+	if (!buf || !ne) {
+		ret_val = -EINVAL;
+		goto out;
+	}
 
-  if (copy_from_user(&size, ne, sizeof(int))) {
-    ret_val = -EFAULT;
-    goto out;
-  }
+	if (copy_from_user(&size, ne, sizeof(int))) {
+		ret_val = -EFAULT;
+		goto out;
+	}
 
-  if (size < 1) {
-    ret_val = -EINVAL;
-    goto out;
-  }
+	if (size < 1) {
+		ret_val = -EINVAL;
+		goto out;
+	}
 
-  read_lock(&tasklist_lock);
-    for_each_process(t) {
-      processes_before++;
-    }
-  read_unlock(&tasklist_lock);
+	read_lock(&tasklist_lock);
+	for_each_process(t) {
+		processes_before++;
+	}
+	read_unlock(&tasklist_lock);
 
 
-  for(int attempts = 0;attempts<10 ; attempts++)
-  {
-    kbuf = kcalloc(processes_before + SLACK, sizeof(struct k22info), GFP_KERNEL);
+	for (int attempts = 0; attempts < MAX_ATTEMPTS; attempts++) {
 
-    if (!kbuf) {
-      ret_val = -ENOMEM;
-      goto out;
-    }
+		kbuf = kcalloc(processes_before + SLACK, sizeof(struct k22info), GFP_KERNEL);
+		if (!kbuf) {
+			ret_val = -ENOMEM;
+			goto out;
+		}
 
-    processes_after = dfs(kbuf,processes_before + SLACK);
+		processes_after = dfs(kbuf,processes_before + SLACK);
 
-    if(processes_after <=  processes_before + SLACK){
-      break;
-    }
-    else{
-      if(attempts == 9){
-        processes_after = processes_before + SLACK;
-        break;
-      }
-        kfree(kbuf);
-      processes_before += 50; 
-    }
-  }
-  kne = min(processes_after,size);
+		if (attempts == MAX_ATTEMPTS - 1) {
+			processes_after = processes_before + SLACK;
+			break;
+		}
 
-  if (copy_to_user(buf, kbuf, kne * sizeof(struct k22info))) {
-    ret_val = -EFAULT;
-    goto out;
-  }
+		if (processes_after <= processes_before + SLACK || size < processes_after )
+			break;
 
-  if (copy_to_user(ne, &kne, sizeof(int))) {
-    ret_val = -EFAULT;
-    goto out;
-  }
+		kfree(kbuf);
+		processes_before += 50;
+	}
 
-    ret_val = processes_after;
+	kne = min(processes_after,size);
+	if (copy_to_user(buf, kbuf, kne * sizeof(struct k22info))) {
+		ret_val = -EFAULT;
+		goto out;
+	}
 
+	if (copy_to_user(ne, &kne, sizeof(int))) {
+		ret_val = -EFAULT;
+		goto out;
+	}
+
+	ret_val = processes_after;
 out:
-  if(kbuf){
-    kfree(kbuf);
-  }
-    return ret_val;
+	if (kbuf)
+		kfree(kbuf);
+	
+	return ret_val;
 }
 
 SYSCALL_DEFINE2(k22tree, struct k22info __user *, buf, int __user *, ne) {
-  return do_k22tree(buf, ne);
+	return do_k22tree(buf, ne);
 }
